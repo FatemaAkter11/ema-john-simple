d8951b61fc30a387dcc9e9c0e4805e3b3dbf6173
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-23 19:02:51 +0600

    cart updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,16 +4,25 @@ import './Cart.css';
 const Cart = (props) => {
     // console.log(props.cart);
     const { cart } = props;
+    // const totalReducer = (previuos, product) => previuos + product.price;
+    // const total = cart.reduce(totalReducer, 0);
+
     let total = 0;
     for (const product of cart) {
         total = total + product.price;
     }
+    const shipping = 15;
+    const tax = (total + shipping) * 10;
+    const grandTotal = total + shipping + tax;
     return (
         <div>
             <h3>Order Summary</h3>
             <h5>Items ordered: {props.cart.length}</h5>
             <br />
-            <p>Total: {total}</p>
+            <p>Total: {total.toFixed(2)}</p>
+            <p>Shipping and Handling: {shipping}</p>
+            <p>tax: {tax.toFixed(2)}</p>
+            <p>Grand Total: {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
