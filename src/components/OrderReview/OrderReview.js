c5c93d6ea5c35c639604154f2ba8e48be1ff2d5f
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-10-01 00:24:44 +0600

    create useCart

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 
 const OrderReview = () => {
     const [products] = useProducts();
+    const [cart] = useCart(products);
     return (
         <div>
             <h1>Products: {products.length}</h1>
+            <h2>Cart: {cart.length}</h2>
             <h2>This is OrderReview</h2>
         </div>
     );
