6907be4e3aa2c5174f288caba5a76f7c9faa9d67
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-30 23:49:08 +0600

    useState and useEffect in a different file

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 
 const OrderReview = () => {
+    const [products] = useProducts();
     return (
         <div>
+            <h1>Products: {products.length}</h1>
             <h2>This is OrderReview</h2>
         </div>
     );
