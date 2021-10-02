657303327f473b22b8e9abf5cf6fbc772a68eb6b
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-10-02 11:09:41 +0600

    update quantity of existing item

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -46,11 +46,21 @@ const Shop = () => {
     }, [products]);
 
     const handleAddToCart = (product) => {
-        // console.log(product);
-        const newCart = [...cart, product];
+        const exists = cart.find(pd => pd.key === product.key);
+        let newCart = [];
+        if (exists) {
+            const rest = cart.filter(pd => pd.key !== product.key);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, product];
+        }
+        else {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
         setCart(newCart);
-        //save to local storage (for now)
+        // save to local storage (for now)
         addToDb(product.key);
+
     }
 
     const handleSearch = event => {
